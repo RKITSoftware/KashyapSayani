13ee27a63f5e06c8bef3d3c713a032e6ab77ad57
KashyapSayani <kashyapsayani1@gmail.com>
2021-10-09 16:41:38 +0530

    Add To Projects Conditional Statement Demo And Boxing And Unboxing Demo

---
+++ Module_1/Code/ConsoleAppBoxingDemo/ConsoleAppBoxingDemo/BoxingDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppBoxingDemo
+{
+    class BoxingDemo
+    {
+        static void Main(string[] args)
+        {
+            // assigned int value
+            // 2020 to num
+            int num = 2020;
+
+            // boxing
+            object obj = num;
+
+            // value of num to be change
+            num = 100;
+
+            System.Console.WriteLine
+            ("Value - type value of num is : {0}", num);
+            System.Console.WriteLine
+            ("Object - type value of obj is : {0}", obj);
+        }
+    }
+}
---
+++ Module_1/Code/ConsoleAppForConditionalStatementDemo/ConsoleAppForConditionalStatementDemo/ConditionalStatement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleAppForConditionalStatementDemo
+{
+    class ConditionalStatement
+    {
+		static void Main()
+		{
+			// local variable declaration
+			int i = 10, j = 20;
+
+			// check if i is greater than j or not
+			if (i > j)
+			{
+				Console.WriteLine("i is greater than j");
+			}
+
+			// if i is less than j or not
+			if (i < j)
+			{
+				Console.WriteLine("i is less than j");
+			}
+
+			// check if i and j both are equal or not
+			if (i == j)
+			{
+				Console.WriteLine("i is equal to j");
+			}
+		}
+	}
+}
